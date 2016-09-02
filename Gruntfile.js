4649d0d1d735923f6b8ad0d525ab603b04dccc9c
Deborah van der Weel <deborahvdw@deborahvdw.choffice.nl>
2016-09-02 15:26:54 +0200

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,61 @@
+module.exports = function(grunt) {
+
+    // 1. All configuration goes here 
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        // Voegt alle files die in js map zitten bij elkaar in main.js
+        concat: {
+            dist: {
+                src: [
+                    'js/libs/*.js', // All JS in the libs folder
+                    'js/main.js'  // This specific file
+                ],
+                dest: 'js/build/total.js',
+            }
+        },
+        // Minimalize total.js - maakt total.min.js aan
+        uglify: {
+            build: {
+                src: 'js/build/total.js',
+                dest: 'js/build/total.min.js'
+            }
+        },
+        compass: {
+            dev: {
+                options: {
+                    sourcemap: false,
+                    sassDir: 'scss/',
+                    specify: ['scss/test.scss'],
+                    cssDir: 'css/',
+                    relativeAssets: false
+                }
+            }
+        },
+        watch: {
+            scripts: {
+                files: ['js/*.js'],
+                tasks: ['concat', 'uglify','compass'],
+                options: {
+                    spawn: false,
+                },
+                compass: {
+                    files: ['Resources/public/scss/{,*/}*.{scss,sass}'],
+                    tasks: ['compass:dev']
+                }
+
+            }
+        }
+
+    });
+
+    // 3. Where we tell Grunt we plan to use this plug-in.
+    grunt.loadNpmTasks('grunt-contrib-compass');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
+    grunt.registerTask('default', ['concat','uglify','compass','watch']);
+
+};
---
+++ js/build/total.js
@@ -0,0 +1,2 @@
+gdfgdsjfhdjghjfhgds
+fddjfhdjhgdsjhshgj
